9cc8d0cdcf89d6ecc58c9f8c223511285f7b814a
Žak Bršek <zak.bbrsek@gmail.com>
2019-09-17 13:16:16 +0200

    Create Spletna ucilnica_mysql_create.sql

---
+++ Spletna ucilnica_mysql_create.sql
@@ -0,0 +1,103 @@
+CREATE TABLE `Ucitelji` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`Ime` varchar(255) NOT NULL AUTO_INCREMENT,
+	`Priimek` varchar(255) NOT NULL AUTO_INCREMENT,
+	`Email` varchar(255) NOT NULL AUTO_INCREMENT,
+	`id_slika` INT NOT NULL,
+	`id_drzava` INT NOT NULL,
+	`id_kraj` INT NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `Moduli` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`Ime` varchar(255) NOT NULL AUTO_INCREMENT,
+	`id_slika` INT NOT NULL,
+	`id_naloga` INT NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `Naloge` (
+	`Id` INT NOT NULL AUTO_INCREMENT,
+	`Ocena` INT NOT NULL AUTO_INCREMENT,
+	`Opis` TEXT NOT NULL AUTO_INCREMENT,
+	`Rok_oddaje` DATETIME NOT NULL AUTO_INCREMENT,
+	`id_datoteka` INT NOT NULL,
+	`id_slika` INT NOT NULL,
+	PRIMARY KEY (`Id`)
+);
+
+CREATE TABLE `Datoteke` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`Url` blob NOT NULL AUTO_INCREMENT,
+	`Tip` varchar(255) NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `Drzave` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`Ime` varchar(255) NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `Uporabniki` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`Ime` varchar(255) NOT NULL AUTO_INCREMENT,
+	`Priimek` varchar(255) NOT NULL AUTO_INCREMENT,
+	`id_slika` INT NOT NULL AUTO_INCREMENT,
+	`id_drzava` INT NOT NULL AUTO_INCREMENT,
+	`id_kraji` INT NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `Kraji` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`Ime` varchar(255) NOT NULL AUTO_INCREMENT,
+	`Postna_st` INT NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `Slike` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`Url` blob NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `Modul_Uporabnik` (
+	`id_modul` INT NOT NULL,
+	`id_uporabnik` INT NOT NULL
+);
+
+CREATE TABLE `Modul_Ucitelj` (
+	`id_ucitelj` BINARY NOT NULL,
+	`id_modul` BINARY NOT NULL
+);
+
+ALTER TABLE `Ucitelji` ADD CONSTRAINT `Ucitelji_fk0` FOREIGN KEY (`id_slika`) REFERENCES `Slike`(`id`);
+
+ALTER TABLE `Ucitelji` ADD CONSTRAINT `Ucitelji_fk1` FOREIGN KEY (`id_drzava`) REFERENCES `Drzave`(`id`);
+
+ALTER TABLE `Ucitelji` ADD CONSTRAINT `Ucitelji_fk2` FOREIGN KEY (`id_kraj`) REFERENCES `Kraji`(`id`);
+
+ALTER TABLE `Moduli` ADD CONSTRAINT `Moduli_fk0` FOREIGN KEY (`id_slika`) REFERENCES `Slike`(`id`);
+
+ALTER TABLE `Moduli` ADD CONSTRAINT `Moduli_fk1` FOREIGN KEY (`id_naloga`) REFERENCES `Naloge`(`Id`);
+
+ALTER TABLE `Naloge` ADD CONSTRAINT `Naloge_fk0` FOREIGN KEY (`id_datoteka`) REFERENCES `Datoteke`(`id`);
+
+ALTER TABLE `Naloge` ADD CONSTRAINT `Naloge_fk1` FOREIGN KEY (`id_slika`) REFERENCES `Slike`(`id`);
+
+ALTER TABLE `Uporabniki` ADD CONSTRAINT `Uporabniki_fk0` FOREIGN KEY (`id_slika`) REFERENCES `Slike`(`id`);
+
+ALTER TABLE `Uporabniki` ADD CONSTRAINT `Uporabniki_fk1` FOREIGN KEY (`id_drzava`) REFERENCES `Drzave`(`id`);
+
+ALTER TABLE `Uporabniki` ADD CONSTRAINT `Uporabniki_fk2` FOREIGN KEY (`id_kraji`) REFERENCES `Kraji`(`id`);
+
+ALTER TABLE `Modul_Uporabnik` ADD CONSTRAINT `Modul_Uporabnik_fk0` FOREIGN KEY (`id_modul`) REFERENCES `Moduli`(`id`);
+
+ALTER TABLE `Modul_Uporabnik` ADD CONSTRAINT `Modul_Uporabnik_fk1` FOREIGN KEY (`id_uporabnik`) REFERENCES `Uporabniki`(`id`);
+
+ALTER TABLE `Modul_Ucitelj` ADD CONSTRAINT `Modul_Ucitelj_fk0` FOREIGN KEY (`id_ucitelj`) REFERENCES `Ucitelji`(`id`);
+
+ALTER TABLE `Modul_Ucitelj` ADD CONSTRAINT `Modul_Ucitelj_fk1` FOREIGN KEY (`id_modul`) REFERENCES `Moduli`(`id`);
+
